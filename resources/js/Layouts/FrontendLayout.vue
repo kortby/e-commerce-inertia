682cece3d7f12ff6333f86e44e5eac9ffd6f85b8
kortby@gmail.com <benk@Bens-MacBook-Pro.local>
2023-10-15 14:06:28 -0500

    first commit

---
+++ resources/js/Layouts/FrontendLayout.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { Head, Link, router } from '@inertiajs/vue3';
+
+defineProps({
+    title: String,
+});
+
+</script>
+
+<template>
+    <div>
+
+        <Head :title="title" />
+
+        <div class="min-h-screen bg-gray-100">
+            <!-- nav -->
+
+            <!-- Page Heading -->
+            <header v-if="$slots.header" class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <slot name="header" />
+                </div>
+            </header>
+
+            <!-- Page Content -->
+            <main>
+                <slot />
+            </main>
+        </div>
+    </div>
+</template>
---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,21 @@
+<script setup>
+import BackendLayout from '@/Layouts/BackendLayout.vue';
+</script>
+
+<template>
+    <BackendLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <h1>Welcome to your E-commerce application!</h1>
+                </div>
+            </div>
+        </div>
+    </BackendLayout>
+</template>
